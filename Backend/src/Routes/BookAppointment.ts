e3145a3c587247d7be9132e9381264c831fe0382
Parth Kachhela <parthkachhela78@gmail.com>
2025-06-03 19:14:10 +0530

    route crated..

--- Backend/src/Routes/BookAppointment.ts
+++ Backend/src/Routes/BookAppointment.ts
@@ -2,7 +2,7 @@ import express, { Response, Request } from "express";
 import { PrismaClient } from "@prisma/client";
 const client = new PrismaClient();
 
-export default async function (req: Request, res: Response) {
+export default async function appointment(req: Request, res: Response) {
   const fullName = req.body.fullName;
   const mobileNumber = req.body.mobileNumber;
   const helpWith = req.body.helpWith;
